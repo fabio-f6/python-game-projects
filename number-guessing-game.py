39d0b1254502a34b27370f8cc01db5a5b522ba62
fabio-f6 <fabio.fbff@icloud.com>
2025-10-06 13:26:10 +0100

    Number Guessing Game with extra features

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -1,33 +1,45 @@
-#loop
-#generate number
-#ask user for input
-#compare input to generated number
-
 import random
 
-def random_number():
-    return random.randint(1, 100)
+def random_number(low, high):
+    return random.randint(low, high)
 
-attempts = 10
+def best_attempts(f_bestattempts, f_attempts):
+    if f_bestattempts == 0:
+        return f_attempts
+    elif attempts < f_bestattempts:
+        return f_attempts
+    else:
+        return f_bestattempts
+
+bestAttempts = 0
 
 while True:
-    answer = random_number()
     minimum_value = int(input("Minimum value: "))
     maximum_value = int(input("Maximum value: "))
-    while True and attempts > 0:
+    answer = random_number(minimum_value, maximum_value)
+    print(f"DEBUG: {answer}")
+    attemptsLeft = 2
+    attempts = 0
+    while True and attemptsLeft > 0:
         try:
             choice = int(input(f"Guess a number between {minimum_value} and {maximum_value}: "))
             if choice == answer:
-                print("Correct!")
+                attempts+=1
+                bestAttempts = best_attempts(bestAttempts, attempts)
+                print(f"Correct! You guessed the number is {attempts} attempts. Your best game was {bestAttempts} attempts.")
                 break
             elif choice > answer:
-                attempts-=1
-                print(f"Too high! {attempts} attempts remaining.")
-            elif choice < answer:
-                print(f"Too low! {attempts} attempts remaining.")
-                attempts-=1
+                attemptsLeft-=1
+                attempts+=1
+                print(f"Too high! {attemptsLeft} attempts remaining.")
+            else:
+                attemptsLeft -= 1
+                attempts+=1
+                print(f"Too low! {attemptsLeft} attempts remaining.")
         except ValueError:
             print("Please enter a number.")
+    if attemptsLeft == 0:
+        print(f"Game Over! The answer was {answer}.")
     answer = input("Play again? (y/n): ")
     if answer == "n":
         break
