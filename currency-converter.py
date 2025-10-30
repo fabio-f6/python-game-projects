4c85c1323032d77a1d1f745148a2acf40331a701
fabio-f6 <fabio.fbff@icloud.com>
2025-10-30 14:53:42 +0000

    Currency Converter with extra features
    Project Handbook also added to base folder.

--- currency-converter.py
+++ currency-converter.py
@@ -1,11 +1,11 @@
 def get_currency(label):
-    choices = ("EUR", "USD", "BRL")
+    choices = ("EUR", "USD", "BRL", "CAD", "GBP")
     while True:
-        currency = input(f"Enter {label} currency (EUR/USD/BRL): ").upper()
+        currency = input(f"Enter {label} currency (EUR/USD/BRL/CAD/GBP): ").upper()
         if currency in choices:
             return currency
         else:
-            print("Please enter a valid currency (EUR/USD/BRL)")
+            print("Please enter a valid currency (EUR/USD/BRL/CAD/GBP)")
 
 def get_amount():
     while True:
@@ -17,24 +17,37 @@ def get_amount():
         except ValueError:
             print("Invalid input. Please enter a positive numeric value")
 
-def convert(inputAmount, inputCurrency, outputCurrency):
+def convert(inputAmount, inputCurrency):
     exchange_rates = {
-        "USD": {"EUR": 0.86, "BRL": 5.39},
-        "EUR": {"USD": 1.16, "BRL": 6.34},
-        "BRL": {"EUR": 0.16, "USD": 0.19},
+        "USD": {"EUR": 0.86, "BRL": 5.39, "GBP": 0.73, "CAD": 1.39},
+        "EUR": {"USD": 1.16, "BRL": 6.34, "GBP": 0.85, "CAD": 1.62},
+        "BRL": {"EUR": 0.16, "USD": 0.19, "GBP": 0.14, "CAD": 0.29},
+        "GBP": {"USD": 1.35, "EUR": 1.17, "BRL": 7.50, "CAD": 1.90},
+        "CAD": {"USD": 0.72, "EUR": 0.62, "BRL": 3.90, "GBP": 0.53},
     }
 
-    if inputCurrency == outputCurrency:
-        return inputAmount
+    results = []
 
-    return inputAmount * exchange_rates[inputCurrency][outputCurrency]
+    for currency in exchange_rates[inputCurrency]:
+        converted_amount = inputAmount * exchange_rates[inputCurrency][currency]
+        results.append(f"{inputAmount} {inputCurrency} to {currency} -> {converted_amount:.2f}")
+
+    return results
 
 def main():
-    input_amount = get_amount()
-    input_currency = get_currency("source")
-    output_currency = get_currency("target")
-    converted_amount = convert(input_amount, input_currency, output_currency)
-    print(f"{input_amount} {input_currency} to {output_currency} -> {converted_amount:.2f}")
+    history = []
+    while True:
+        input_amount = get_amount()
+        input_currency = get_currency("source")
+
+        history.append(convert(input_amount, input_currency))
+        print(history[-1])
+        if "n" in input("Type 'n' if you want to stop the program.").lower():
+            print("Conversion history:")
+            for record in history:
+                print(record)
+            break
+
 
 if __name__ == '__main__':
     main()
